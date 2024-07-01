f7e88b1ef0c9075a7e0e820da6cc6bc75172c7f7
clement <clement.dreiski@gmail.com>
2024-07-01 11:54:22 +0200

    [+] GitHub release workflow

--- SimpleRepeat/obj/Release/net8.0/SimpleRepeat.AssemblyInfo.cs
+++ SimpleRepeat/obj/Release/net8.0/SimpleRepeat.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleRepeat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cc9cb62d4b5853bae8c14f6197f4928c1eceb9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+205a287751ae67449783744d846787cac25e5f70")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleRepeat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleRepeat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
